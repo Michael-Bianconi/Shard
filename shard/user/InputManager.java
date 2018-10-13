ed050a402486f1a76d0060ea67fdf31dfcdadda9
Michael-Bianconi <michael.a.bianconi@gmail.com>
2018-10-13 17:50:42 -0400

    Overhauling the InputManager. Created a better parseCommand method.

--- shard/user/InputManager.java
+++ shard/user/InputManager.java
@@ -1,211 +1,105 @@
-/**
- * Shard
- * @author Michael Bianconi
- *
- * Right now this manager is pretty janky. I'll come back and clean it up.
- * As of right now, it only accepts input consisting of a one-word command
- * and a single argument (although the argument may be several words).
- *
- * Additionally, the user is forced to use full names.
- *
- * Examples:
- *
- * Valid:
- *
- *      ENTER kitchen
- *      drop rope
- *      talk fred barnes
- *
- * Invalid:
- *
- *      go to kitchen
- *      drop rope (when only a bloody rope is in the player's inventory)
- *      talk to fred barnes
- *      talk fred
- */
 package shard.user;
 import shard.event.Event;
 import shard.event.EventCode;
-import shard.object.*;
-import java.util.ArrayList;
-import java.util.Arrays;
+import shard.object.Player;
+import java.util.Map;
+import java.util.HashMap;
 
+/**
+ * Shard
+ * @author Michael Bianconi
+ * @author https://www.github.com/Michael-Bianconi
+ *
+ * The InputManager takes input and turns into Shard-readable Events.
+ * The main difficult is efficiently parsing input.
+ */
 public class InputManager {
 
-    // MEMBER VARIABLES ========================================================
+    private static final Map<String, EventCode> codeMap;
+    static {
+        codeMap = new HashMap<String, EventCode>();
 
-    private Player player;
+        codeMap.put("ENTER",       EventCode.ENTER);
+        codeMap.put("GOTO",        EventCode.ENTER);
+        codeMap.put("GO TO",       EventCode.ENTER);
 
-    // CONSTRUCTOR =============================================================
+        codeMap.put("DESCRIBE",    EventCode.DESCRIBE);
+        codeMap.put("HELP",        EventCode.DESCRIBE);
+        codeMap.put("INFORMATION", EventCode.DESCRIBE);
+        codeMap.put("INFO",        EventCode.DESCRIBE);
 
-    public InputManager(Player p) {
-        this.player = p;
-    }
+        codeMap.put("TAKE",        EventCode.TAKE);
+        codeMap.put("PICKUP",      EventCode.TAKE);
+        codeMap.put("PICK UP",     EventCode.TAKE);
 
-    // INPUT PARSING ===========================================================
+        codeMap.put("DROP",        EventCode.DROP);
+        codeMap.put("LEAVE",       EventCode.DROP);
 
-    /**
-     * Given a String command, ascertain its corresponding EventCode. Then, if
-     * applicable, build the rest of the Event using information gleened from
-     * this instance's Player. For example, if the command specifies to take
-     * a knife, look around the Player's location for a knife.
-     *
-     * When identifying Items, it compares the toString() version of the item.
-     */
-    public Event parse(String input) throws CommandNotFoundException,
-                                            ArgumentNotFoundException {
+        codeMap.put("INVESTIGATE", EventCode.INVESTIGATE);
+        codeMap.put("LOOK AROUND", EventCode.INVESTIGATE);
 
-        if (input.length() == 0) { throw new CommandNotFoundException(
-                                             "Empty command!"); }
-
-        // arr[0] = command
-        // arr[1] = argument
-        String [] arr = input.split(" ", 2);
-        String command = arr[0];
-        String argument = (arr.length == 2) ? arr[1] : "";
-        EventCode c;
-
-        // Read in the event code or return an Error
-        try {
-            c = EventCode.valueOf(command.toUpperCase());
-        } catch (IllegalArgumentException e) {
-            throw new CommandNotFoundException("Unknown command!");
-        }
-
-        ArrayList<ShardObject> objectList;
-
-        // parse the command arguments
-        try {
-            // args should be limited to Rooms
-            switch (c) {
-
-                // Look for connected rooms
-                case ENTER: // drop down to INVESTIGATE
-                case INVESTIGATE: objectList = roomParse(argument);
-                    break;
-
-                // Look for Item held by the player
-                case DROP: objectList = itemOnPlayerParse(argument);
-                    break;
-
-                // Look for Item in current room.
-                case TAKE: objectList = itemInRoomParse(argument);
-                    break;
-
-                // Look for Guest in current room.
-                case TALK: objectList = guestParse(argument);
-                    break;
-
-                // Look for anything
-                case DESCRIBE: objectList = allObjectParse(argument);
-                    break;
-
-                // The user used ERROR as the command for some reason
-                case ERROR: objectList = new ArrayList<ShardObject>();
-                    break;
-
-                // Default (should never happen)
-                default:
-                    throw new CommandNotFoundException("Dev error");
-            }
-        } catch (ArrayIndexOutOfBoundsException e) {
-            throw new ArgumentNotFoundException("Argument not found!");
-        }
-
-        return new Event(c, this.player, objectList);
+        codeMap.put("TALK",        EventCode.TALK);
+        codeMap.put("TALK TO",     EventCode.TALK);
+        codeMap.put("TALK WITH",   EventCode.TALK);
     }
 
     /**
-     * Look through the given list of objects and match it to the given name.
-     * Matches are determined through comparing the name to the ShardObject's
-     * toString() method.
+     * This method takes user input (e.g. "describe lounge"),
+     * and turns it into an Event (e.g. DESCRIBE PLAYER LOUNGE).
+     * Only ShardObjects relevant to the player are considered.
      *
-     * Only ShardObject's that share the same class as the filter may be
-     * matched.
-     *
-     * @param name Name to match.
-     * @param filter Class to match.
-     * @param objs Possible ShardObjects.
-     * @return ArrayList of ShardObject.
+     * @param p Player object.
+     * @param input String input.
+     * @return Shard-readable Event.
+     * @exception InvalidInputException
      */
-    private ArrayList<ShardObject> objectParse(String name,
-                                               Class filter,
-                                               ArrayList<ShardObject> objs)
-                                          throws ArgumentNotFoundException {
+    public static String parse(Player p, String input)
+        throws InvalidInputException {
 
-        ArrayList<ShardObject> parsedArgs = new ArrayList<ShardObject>();
-        
-        // for each ShardObject in the supplied list
-        for (ShardObject o : objs) {
+        return parseCommand(input);
 
-            // if the object is of the correct type
-            if (filter.isInstance(o)) {
+    }
 
-                if (name.toUpperCase().equals(o.toString().toUpperCase())) {
-                    parsedArgs.add(o);
-                    return parsedArgs;
-                }
+    /**
+     * Given the input, try to determine the command. If it gets found,
+     * return a new input string with the Shard-readable command.
+     * For example, "talk to fred barnes" becomes "TALK fred barnes".
+     * This method will return the first code match. For example,
+     * "talk talk to fred barnes" with return "TALK talk to fred barnes".
+     *
+     * See parse() for possible issues.
+     *
+     * @param input String to parse.
+     * @return New String with a Shard-readable command.
+     * @exception InvalidInputException if the command is invalid.
+     * @see parse()
+     */
+    private static String parseCommand(String input)
+        throws InvalidInputException {
+
+        String[] tokens = input.split(" ");
+
+        String sub = "";
+        int commandEndIndex = 0;
+        EventCode code = null;
+
+        // get the longest substring, starting at 0, that matches a command
+        for (String t : tokens) {
+            sub += t.toUpperCase();
+            if (commandMap.containsKey(sub)) {
+                code = commandMap.get(sub);
+                commandEndIndex = sub.length();
             }
+            sub += " ";
         }
 
-        // no corresponding ShardObject was found.
-        throw new ArgumentNotFoundException("Can't find " + name);
-    }
-
-    /** Call objectParse on the current room and all connected rooms. */
-    private ArrayList<ShardObject> roomParse(String arg)
-        throws ArgumentNotFoundException {
-
-        ArrayList<ShardObject> list = new ArrayList<ShardObject>();
-
-        if (player.getLocation() instanceof Room) {
-            Room location = (Room) player.getLocation();
-            list.add(location);
-            list.addAll(location.getConnectedRooms());
+        // no such substring was found
+        if (code == null) {
+            throw new InvalidInputException("Unrecognized command!");
         }
 
-        return objectParse(arg, Room.class, list);
-    }
-
-    /** objectParse on all items in the player's inventory. */
-    private ArrayList<ShardObject> itemOnPlayerParse(String arg)
-        throws ArgumentNotFoundException {
-
-        return objectParse(arg, Item.class, this.player.getObjects());
-    }
-
-    /** Call objectParse on all items in the current room. */
-    private ArrayList<ShardObject> itemInRoomParse(String arg)
-        throws ArgumentNotFoundException {
-
-        return objectParse(arg, Item.class, player.getLocation().getObjects());
-    }
-
-    /** Call objectParse on all people in the current room. */
-    private ArrayList<ShardObject> guestParse(String arg)
-        throws ArgumentNotFoundException {
-
-        return objectParse(arg, Guest.class, player.getLocation().getObjects());
-    }
-
-    /** Call objectParse on all ShardObjects on the player and in the room. */
-    private ArrayList<ShardObject> allObjectParse(String arg)
-        throws ArgumentNotFoundException {
-
-        ArrayList<ShardObject> list = new ArrayList<ShardObject>();
-
-        list.addAll(player.getObjects());
-
-        if (player.getLocation() instanceof Room) {
-            System.out.println("true");
-            Room location = (Room) player.getLocation();
-            list.add(location);
-            list.addAll(location.getObjects());
-            list.addAll(location.getConnectedRooms());
-        }
-
-        list.add(player);
-        
-        return objectParse(arg, ShardObject.class, list);
+        // return a more Shard-readable version of the input
+        // e.g. "go to lounge" becomes "ENTER lounge"
+        return code.name() + input.substring(commandEndIndex, input.length());
     }
 }
