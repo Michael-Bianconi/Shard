e1ef017055b153d70965f4b40f337ebec90da2a1
Michael-Bianconi <michael.a.bianconi@gmail.com>
2018-10-20 00:00:26 -0400

    house cleaning

--- shard/event/Command.java
+++ shard/event/Command.java
@@ -12,7 +12,10 @@ import java.util.Random;
  */
 public enum Command {
 
+    /** Enter another room (does not have to be connected). */
     ENTER {
+
+        /** Build a list of connected rooms. */
         @Override
         public ArrayList<ShardObject> buildCandidateList(ShardObject target) {
 
@@ -22,15 +25,21 @@ public enum Command {
             return filter(Room.class, list);
         }
 
+        /** Takes one argument. */
         @Override
         public int getNumArguments() { return 1; }
 
+        /**
+         * Sets the executor's location to the given location.
+         * Location must be an Owner.
+         */
         @Override
         public void execute(ShardObject executor, ShardObject location) {
 
             executor.setLocation((Owner) location);
         }
 
+        /** Guests will remember this. */
         @Override
         public boolean remember() { return true; }
 
@@ -38,7 +47,10 @@ public enum Command {
         public String pastTense() { return "entered"; }
     },
 
+    /** Print out a description (the executor has no impact on the result). */
     DESCRIBE {
+
+        /** Literally every ShardObject the target has access to. */
         @Override
         public ArrayList<ShardObject> buildCandidateList(ShardObject target) {
 
@@ -56,9 +68,11 @@ public enum Command {
             return list;
         }
 
+        /** Takes one argument */
         @Override
         public int getNumArguments() { return 1; }
 
+        /** Print out the description of the given object. */
         @Override
         public void execute(ShardObject executor, ShardObject item) {
             System.out.println(
@@ -70,15 +84,22 @@ public enum Command {
         @Override
         public String pastTense() { return "described"; }
 
+        @Override
+        public boolean consumesAction() { return false; }
+
     },  
 
+    /** Pick up an object from the environment. Must be movable. */
     TAKE {
+
+        /** List of all movable objects in the target's location. */
         @Override
         public ArrayList<ShardObject> buildCandidateList(ShardObject target) {
 
             ArrayList<ShardObject> items = new ArrayList<ShardObject>();
+            ArrayList<ShardObject> inRoom = target.getLocation().getObjects();
         
-            for (ShardObject i : filter(Item.class, target.getLocation().getObjects())) {
+            for (ShardObject i : filter(Item.class, inRoom)) {
                 Item item = (Item) i;
                 if (item.getMovable()) { items.add(i); }
             }
@@ -86,13 +107,18 @@ public enum Command {
             return items;
         }
 
+        /** Takes one argument. */
         @Override
         public int getNumArguments() { return 1; }
 
+        /**
+         * Set the item's location to be the executor. The executor must be
+         * an Owner.
+         */
         @Override
         public void execute(ShardObject executor, ShardObject item) {
-            Item i = (Item) item;
-            if (i.getMovable()) { item.setLocation((Owner) executor); }
+   
+            item.setLocation((Owner) executor);
         }
 
         @Override
@@ -102,7 +128,10 @@ public enum Command {
         public String pastTense() { return "took"; }
     },
 
+    /** Remove an item from the target's inventory. */
     DROP {
+
+        /** Build a list of movable items in the target's inventory. */
         @Override
         public ArrayList<ShardObject> buildCandidateList(ShardObject target) {
 
@@ -120,11 +149,11 @@ public enum Command {
         @Override
         public int getNumArguments() { return 1; }
 
+        /** The item in the executor's location's inventory. */
         @Override
         public void execute(ShardObject executor, ShardObject item) {
             Item i = (Item) item;
-            if (i.getMovable()) { item.setLocation(executor.getLocation()); }
-            else { System.out.println("immovable object."); }
+            item.setLocation(executor.getLocation());
         }
 
         @Override
@@ -135,12 +164,16 @@ public enum Command {
 
     },
 
+    /** Interact with an object. */
     USE {
+
+        /** Build a list of usable objects. */
         @Override
         public ArrayList<ShardObject> buildCandidateList(ShardObject target) {
             ArrayList<ShardObject> list = new ArrayList<ShardObject>();
+            ArrayList<ShardObject> inRoom = target.getLocation().getObjects();
 
-            for (ShardObject i : filter(Item.class, target.getLocation().getObjects())){
+            for (ShardObject i : filter(Item.class, inRoom)) {
                 Item item = (Item) i;
                 if (item.getUsable()) { list.add(i); }
             }
@@ -157,6 +190,7 @@ public enum Command {
         @Override
         public int getNumArguments() { return 1; }
 
+        /** Get the Item's onUse event and execute it. */
         @Override
         public void execute(ShardObject executor, ShardObject item) {
             Item i = (Item) item;
@@ -172,10 +206,16 @@ public enum Command {
 
     },
 
+    /** Kill a guest. */
     KILL {
+
+        /** Build a list of all people in the room excluding the target. */
         @Override
         public ArrayList<ShardObject> buildCandidateList(ShardObject target) {
-            ArrayList<ShardObject> list = Command.TALK.buildCandidateList(target);
+
+            ArrayList<ShardObject> list =
+                Command.TALK.buildCandidateList(target);
+
             list.remove(target);
             return list;
         }
@@ -188,15 +228,26 @@ public enum Command {
         public void execute(ShardObject executor, ShardObject item) {
 
             Guest guest = (Guest) item;
+            Owner location = item.getLocation();
+            String description = "Here lies " + guest.getName() + ", dead.";
+
+
             guest.setDead(true);
-            Item deadGuest = new Item(guest.getName(), "he's dead, jim", item.getLocation());
+            Item deadGuest = new Item(guest.getName(), description, location);
             guest.setLocation(new Room());
         }
 
+        @Override
+        public boolean remember() { return true; }
 
+        @Override
+        public String pastTense() { return "killed"; }
     },
 
+    /** Talk to a guest. Player only. */
     TALK {
+
+        /** Build a list of Guests in the target's room. */
         @Override
         public ArrayList<ShardObject> buildCandidateList(ShardObject target) {
 
@@ -208,6 +259,7 @@ public enum Command {
         @Override
         public int getNumArguments() { return 1; }
 
+        /** Have a Conversation with the guest. */
         @Override
         public void execute(ShardObject executor, ShardObject item) {
             Memory guest = (Memory) item;
@@ -218,25 +270,10 @@ public enum Command {
         public String pastTense() { return "talked to"; }
     },
 
-    TALK_TO_PLAYER {
-        @Override
-        public ArrayList<ShardObject> buildCandidateList(ShardObject target) {
-            ArrayList<ShardObject> list = new ArrayList<ShardObject>();
-            list.addAll(target.getLocation().getObjects());
-            return filter(Player.class, list);
-        }
-
-        @Override
-        public void execute(ShardObject executor, ShardObject item) {
-            Player p = (Player) item;
-            // stuff
-        }
-
-        @Override
-        public String pastTense() { return "talked to"; }
-    },
-
+    /** Print out the Items in the room. */
     INVESTIGATE {
+
+        /** Print out all the items in the room. */
         @Override
         public void execute(ShardObject executor, ShardObject item) {
 
@@ -251,7 +288,10 @@ public enum Command {
 
     }, 
 
+    /** Print out who this is. */
     WHOAMI {
+
+        /** Print the name of the executor. */
         @Override
         public void execute(ShardObject executor, ShardObject item) {
 
@@ -260,10 +300,15 @@ public enum Command {
                                      executor.getName())
             );
         }
+
+        @Override
+        public boolean consumesAction() { return false; }
     },
 
+    /** Print out where this is. */
     WHEREAMI {
 
+        /** Print out this executor's location. */
         @Override
         public void execute(ShardObject executor, ShardObject item) {
 
@@ -273,10 +318,15 @@ public enum Command {
                                      o.getName())
             );
         }
+
+        @Override
+        public boolean consumesAction() { return false; }
     },
 
+    /** Print out the objects held by this person. */
     INVENTORY {
 
+        /** Print out the executor's inventory. */
         @Override
         public void execute(ShardObject executor, ShardObject item) {
 
@@ -289,8 +339,12 @@ public enum Command {
                 );
             }
         }
+
+        @Override
+        public boolean consumesAction() { return false; }
     },
 
+    /** Quit the game. */
     QUIT {
         @Override
         public void execute(ShardObject executor, ShardObject item) {
@@ -298,7 +352,11 @@ public enum Command {
         }
     },
 
-    ERROR;
+    ERROR {
+
+        @Override
+        public boolean consumesAction() { return false; }
+    };
 
     /**
      * Given a player, create an ArrayList of all possible candidates that
@@ -363,4 +421,10 @@ public enum Command {
      * The past tense of the command. By default, the lowercase name.
      */
     public String pastTense() { return name().toLowerCase(); }
+
+
+    /**
+     * Does this command constitute as an action (default yes).
+     */
+    public boolean consumesAction() { return true ; }
 }
