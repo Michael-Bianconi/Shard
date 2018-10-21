7543efc9644f4f4660925151ca61936a7a773b64
Michael-Bianconi <michael.a.bianconi@gmail.com>
2018-10-20 22:21:44 -0400

    Clean up Player

--- shard/object/Player.java
+++ shard/object/Player.java
@@ -1,6 +1,9 @@
 /**
- * Shard
+ * Controlled by the user.
+ *
  * @author Michael Bianconi
+ * @author https://www.github.com/Michael-Bianconi
+ * @version 1
  */
 
 package shard.object;
@@ -16,14 +19,30 @@ public class Player extends ShardObject implements Owner {
 
     // CONSTRUCTOR =============================================================
 
+    /**
+     * Name-only constructor, constructs Player in an empty room with
+     * description set to name.
+     * @param name Name of the player.
+     */
     public Player(String name) {
         this(name, name, new Room());
     }
 
+
+    /**
+     * Name and description constructor, constructs player in an empty room.
+     * @param name Name of the player.
+     * @param description Description of the player.
+     */
     public Player(String name, String description) {
         this(name, description, new Room());
     }
 
+    /**
+     * Name and location constructor, constructs with description set to name.
+     * @param name Name of the player.
+     * @param location Location of the player.
+     */
     public Player(String name, Owner location) {
         this(name, name, location);
     }
@@ -43,9 +62,28 @@ public class Player extends ShardObject implements Owner {
 
     // ACCESSORS ===============================================================
 
-
+    /**
+     * Gets an ArrayList of the objects in this player's inventory.
+     * @return Returns an ArrayList of the ShardObjects.
+     */
     public ArrayList<ShardObject> getObjects() { return inventory; }
+
+    /**
+     * Adds an object to this player's inventory.
+     * @param o Object to add.
+     */
     public void addObject(ShardObject o) { inventory.add(o); }
+
+    /**
+     * Removes an object from this player's inventory.
+     * @param o Object to remove.
+     */
     public void removeObject(ShardObject o) { inventory.remove(o); }
+
+    /**
+     * Checks if the given object is in this player's inventory.
+     * @param o Object to check.
+     * @return Returns true if o is in this player's inventory.
+     */
     public boolean hasObject(ShardObject o) { return inventory.contains(o); }
 }
