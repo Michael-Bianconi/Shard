d934794e41dedde1456dccece02a86031d7a05b7
Michael-Bianconi <michael.a.bianconi@gmail.com>
2018-10-20 22:05:56 -0400

    ShardObject documentation

--- shard/object/ShardObject.java
+++ shard/object/ShardObject.java
@@ -1,15 +1,14 @@
 /**
- * Shard
- * @author Michael Bianconi
- *
  * A ShardObject represents everything in the Shard universe, similar to how
  * Object represents everything in Java.
  *
  * The player specifies ShardObjects by name. Therefore, there should be no
  * chance of collision between the names of two ShardObjects.
  *
- * If two ShardObjects share the same name, they should be functionally
- * identical.
+ * @author Michael Bianconi
+ * @author https://www.github.com/Michael-Bianconi
+ * @version 1
+ * @since 13 October 2018
  */
 package shard.object;
 
@@ -33,14 +32,29 @@ public class ShardObject {
 
     // CONSTRUCTORS ============================================================
 
+    /**
+     * Name-only constructor. Calls this(name, name, null).
+     * @param name Name of the object.
+     */
     public ShardObject(String name) {
         this(name, name, null);
     }
 
+
+    /**
+     * Name and description constructor. Constructs with null location.
+     * @param name Name of this object.
+     * @param description Description of this object.
+     */
     public ShardObject(String name, String description) {
         this(name, description, null);
     }
 
+    /**
+     * Name and location constructor. Constructs with description set to name.
+     * @param name Name of the object.
+     * @param location Location of the object.
+     */
     public ShardObject(String name, Owner location) {
         this(name, name, location);
     }
@@ -64,13 +78,34 @@ public class ShardObject {
     }
 
     // ACCESSORS ===============================================================
+
+    /**
+     * Gets this object's name.
+     * @return name
+     */
     public String getName() { return this.name; }
+
+    /**
+     * Gets this object's description.
+     * @return description
+     */
     public String getDescription() { return this.description; }
+
+    /**
+     * Gets this object's location.
+     * @return location
+     */
     public Owner getLocation() { return this.location; }
+
+    /**
+     * Gets this object's refID.
+     * @return refID
+     */
     public int getRefID() { return this.refID; }
 
     /**
-     * Set the location of this object.
+     * Set the location of this object. Will remove it from
+     * it's current location.
      * @param owner Owner to set.
      */
     public void setLocation(Owner owner) {
@@ -82,12 +117,27 @@ public class ShardObject {
     }
 
     // OBJECT METHODS ==========================================================
+
+    /**
+     * Returns String representation of this object.
+     * @return name
+     */
     @Override
     public String toString() { return this.name; }
 
+    /**
+     * hashCode of this object.
+     * @return refID
+     */
     @Override
     public int hashCode() { return this.refID; }
 
+
+    /**
+     * Checks refID of this object and other.
+     * @param other Other object to check.
+     * @return True if matching refIDs.
+     */
     @Override
     public boolean equals(Object other) {
         if (this == other) { return true; }
